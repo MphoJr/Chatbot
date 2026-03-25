b553ac9ac9e5aa562e127ed684eb201954798001
MphoJr <junior.maudu16@gmail.com>
2026-03-25 15:38:25 +0200

    remove FAQ and university controllers and their routes

--- src/routes/universityRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const universityController = require("../controllers/universityController");
-
-router.get("/", universityController.getUniversities);
-router.get("/:name", universityController.getUniversityByName);
-
-module.exports = router;
